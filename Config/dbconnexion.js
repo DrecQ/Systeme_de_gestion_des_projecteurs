17a3f7c4d3be6a6f8903b737bc9b3b9c37909c8e
DrecQ <drecquist@gmail.com>
2025-02-20 14:54:28 +0100

    [Etudiant 1] Test de la connexion à la base de connées réussie

---
+++ Config/dbconnexion.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Create the connection pool. The pool-specific settings are the defaults
+const dbConnexion = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  port:process.env.DB_PORT, 
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+export default dbConnexion;
--- server.mjs
+++ server.mjs
@@ -1,20 +1,38 @@
-import { createServer } from 'node:http';
+import dbConnexion from './Config/dbconnexion.js';
 import express from 'express';
 import dotenv from 'dotenv';
+
+dotenv.config();
 
 //Initialisation de notre application avec express
 const app = express();
 
-
 //Middlewares
 app.use(express.json());
 
+//Routes 
+app.get('/', (req, res) => {
+  res.status(200).send('Serveur en cours d\'execution');
+});
+
+app.get('/db', async (req, res) => {
+    try {
+      const [rows] = await dbConnexion.query('SELECT 1');
+      res.json({ message: 'Connexion MySQL réussie !', result: rows });
+    } catch (error) {
+      res.status(500).json({ error: 'Erreur de connexion à MySQL', details: error.message});
+    }
+  });
+
+ 
+
+
 
 //Gestion du port 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // starts a simple http server locally on port 3000
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(port, '127.0.0.1', () => {
   console.log('Notre application a bien démarré : 127.0.0.1:3000');
 });
 
